2f796d13b2981b1b9b4378e6cd31dfc5593cea2d
Charles Krook <charles.krook@gmail.com>
2021-11-03 13:32:26 +0100

    created class for todos

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
+// Toggle gridview/listview feature
 let listView = document.getElementById("list-view");
 let gridView = document.getElementById("grid-view");
-
 window.onload = function () {
   listView.addEventListener("click", toggleView);
   gridView.addEventListener("click", toggleView);
 };
-
 function toggleView(e) {
   if (e.target.id == "list-view") {
     e.target.classList.add("active");
@@ -17,5 +16,14 @@ function toggleView(e) {
   }
 }
 
-// listView.classList.add("active");
-// gridView.classList.remove("active");
+// Todo functionality
+
+class Todo {
+  constructor(text, dateCreated, deadLine, priority, done) {
+    this.text = text;
+    this.dateCreated = new Date();
+    this.deadLine = text;
+    this.priority = text;
+    this.done = false;
+  }
+}
