7bfa8ab47c90d7dd4d4d72e93cbe7ae1ebfdc76b
Charles Krook <charles.krook@gmail.com>
2021-11-03 13:50:56 +0100

    removed junk js comment

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-// Toggle gridview/listview feature
 let listView = document.getElementById("list-view");
 let gridView = document.getElementById("grid-view");
+
 window.onload = function () {
   listView.addEventListener("click", toggleView);
   gridView.addEventListener("click", toggleView);
 };
+
 function toggleView(e) {
   if (e.target.id == "list-view") {
     e.target.classList.add("active");
@@ -13,17 +14,5 @@ function toggleView(e) {
   if (e.target.id == "grid-view") {
     e.target.classList.add("active");
     listView.classList.remove("active");
-  }
-}
-
-// Todo functionality
-
-class Todo {
-  constructor(text, dateCreated, deadLine, priority, done) {
-    this.text = text;
-    this.dateCreated = new Date();
-    this.deadLine = text;
-    this.priority = text;
-    this.done = false;
   }
 }
