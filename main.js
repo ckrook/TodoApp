16474c587e20c6a20d19403a3287ee983e4ec0c0
Charles Krook <charles.krook@gmail.com>
2021-11-03 14:27:46 +0100

    added class to todo

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 let listView = document.getElementById("list-view");
 let gridView = document.getElementById("grid-view");
 
+//submit todo
+let todoArray = [];
+let submit = document.getElementById("submit");
+
 window.onload = function () {
   listView.addEventListener("click", toggleView);
   gridView.addEventListener("click", toggleView);
+
+  submit.addEventListener("click", submitTodo).then(console.log(todoArray));
 };
 
 function toggleView(e) {
@@ -14,5 +20,24 @@ function toggleView(e) {
   if (e.target.id == "grid-view") {
     e.target.classList.add("active");
     listView.classList.remove("active");
+  }
+}
+
+// Todo functionality
+
+function submitTodo() {
+  let text = document.getElementById("todo-text").value;
+  let priority =
+    document.getElementById("priority").options[
+      document.getElementById("priority").selectedIndex
+    ].value;
+  todoArray = new todo(text, priority);
+}
+
+class todo {
+  constructor(text, priority) {
+    this.text = text;
+    this.priority = priority;
+    this.done = false;
   }
 }
