0560ff21e0cc816ff22b034fe3ba0004540e216e
Charles Krook <charles.krook@gmail.com>
2021-11-04 06:41:49 +0100

    feature created to add new todo tasks

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+class todo {
+  constructor(id, text, priority, currentDate, deadline) {
+    this.id = id;
+    this.text = text;
+    this.priority = priority;
+    this.created = currentDate;
+    this.deadline = deadline;
+    this.done = false;
+  }
+}
+
 let listView = document.getElementById("list-view");
 let gridView = document.getElementById("grid-view");
 
@@ -8,8 +19,7 @@ let submit = document.getElementById("submit");
 window.onload = function () {
   listView.addEventListener("click", toggleView);
   gridView.addEventListener("click", toggleView);
-
-  submit.addEventListener("click", submitTodo).then(console.log(todoArray));
+  submit.addEventListener("click", submitTodo);
 };
 
 function toggleView(e) {
@@ -23,21 +33,55 @@ function toggleView(e) {
   }
 }
 
-// Todo functionality
+// Add todo item
 
 function submitTodo() {
   let text = document.getElementById("todo-text").value;
+  let deadLine = document.getElementById("deadline").value;
+  let today = new Date();
+  let currentDate =
+    today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
   let priority =
     document.getElementById("priority").options[
       document.getElementById("priority").selectedIndex
     ].value;
-  todoArray = new todo(text, priority);
+  let id = todoArray.length;
+  let temp = new todo(id, text, priority, currentDate, deadLine);
+  todoArray.push(temp);
+  console.log(todoArray);
+
+  let tasks = document.getElementById("tasks");
+  let tempTodo = ``;
+  for (i = 0; i < todoArray.length; i++) {
+    tempTodo = `
+    <div class="task mb-2">
+	    <div class="row">
+
+        <div class="col-2 d-flex justify-content-center align-items-center">
+          <input id="checkbox-${i}" type="checkbox" class="checkmark" />
+        </div>
+
+		    <div class="col-7">
+		    	<p>${todoArray[i].text}</p>
+			    <time>${todoArray[i].deadline}</time>
+		    </div>
+
+        <div class="col-3 d-flex align-items-end">
+          <span class="priority ${todoArray[i].priority}">${todoArray[i].priority}</span>
+        </div>
+
+ 	    </div>
+    </div>
+    `;
+  }
+  tasks.innerHTML += tempTodo;
 }
 
-class todo {
-  constructor(text, priority) {
-    this.text = text;
-    this.priority = priority;
-    this.done = false;
-  }
+// remove todo item
+let checkbox = document.getElementsByClassName("checkmark");
+
+console.log(checkbox[0]);
+
+function removeTodo(e) {
+  console.log(e);
 }
