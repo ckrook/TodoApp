f8ea16584f4a07ed3437483fe6ced01bb1debe34
Charles Krook <charles.krook@gmail.com>
2021-11-09 09:43:13 +0100

    Added some todo functionality

--- main.js
+++ main.js
@@ -16,10 +16,19 @@ let gridView = document.getElementById("grid-view");
 let sortBy = document.getElementById("sortview");
 let submit = document.getElementById("submit");
 let taskWrapper = document.getElementById("tasks");
+let tasksDone = document.getElementById("tasksdone");
+let task = document.getElementsByClassName("task");
 let input = document.getElementById("todo-text");
 let todoArray = [];
 
 window.onload = function () {
+  let storedTodo = localStorage.getItem("todo");
+  storedTodo = JSON.parse(storedTodo);
+  if (storedTodo != null) {
+    printTodos(storedTodo);
+  } else {
+  }
+
   listView.addEventListener("click", toggleView);
   gridView.addEventListener("click", toggleView);
   submit.addEventListener("click", submitTodo);
@@ -27,18 +36,17 @@ window.onload = function () {
     // console.log("helo");
     event.preventDefault();
     if (event.key === "Enter") {
-      console.log("helo");
       return submitTodo();
     }
   });
   sortBy.addEventListener("change", changeSortBy);
-  taskWrapper.addEventListener("click", (event) => {
-    const isInput = event.target.nodeName === "INPUT";
-    if (!isInput) {
-      return;
-    }
-    removeTodos(event);
-  });
+  // taskWrapper.addEventListener("click", (event) => {
+  //   const isInput = event.target.nodeName === "INPUT";
+  //   if (!isInput) {
+  //     return;
+  //   }
+  //   removeTodos(event);
+  // });
 };
 
 function toggleView(e) {
@@ -49,6 +57,9 @@ function toggleView(e) {
   if (e.target.id == "grid-view") {
     e.target.classList.add("active");
     listView.classList.remove("active");
+    taskWrapper.classList.add("container");
+
+    task.classList.add("col-4");
   }
 }
 
@@ -59,7 +70,17 @@ function submitTodo() {
   let deadLine = document.getElementById("deadline").value;
   let today = new Date();
   let currentDate =
-    today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+    today.getFullYear() +
+    "-" +
+    (today.getMonth() + 1) +
+    "-" +
+    today.getDate() +
+    " " +
+    today.getHours() +
+    ":" +
+    today.getMinutes() +
+    ":" +
+    today.getSeconds();
   let priority =
     document.getElementById("priority").options[
       document.getElementById("priority").selectedIndex
@@ -67,9 +88,8 @@ function submitTodo() {
   let id = todoArray.length;
   let temp = new todo(id, text, priority, currentDate, deadLine);
   todoArray.push(temp);
-  console.log(todoArray);
 
-  let tasks = document.getElementById("tasks");
+  document.getElementById("todo-text").value = "";
   printTodos(todoArray);
 }
 
@@ -90,18 +110,21 @@ function changeSortBy() {
     todoArray.sort(function (a, b) {
       return new Date(a.created) - new Date(b.created);
     });
+    saveLocalstorage(todoArray);
     printTodos(todoArray);
   }
   function sortByDeadline() {
     todoArray.sort(function (a, b) {
       return new Date(a.deadline) - new Date(b.deadline);
     });
+    saveLocalstorage(todoArray);
     printTodos(todoArray);
   }
   function sortByPriority() {
     todoArray.sort(function (a, b) {
       return b.priority - a.priority;
     });
+    saveLocalstorage(todoArray);
     printTodos(todoArray);
   }
   function sortByAlphabetical() {
@@ -110,6 +133,7 @@ function changeSortBy() {
       if (a.text.toLowerCase() > b.text.toLowerCase()) return 1;
       return 0;
     });
+    saveLocalstorage(todoArray);
     printTodos(todoArray);
   }
 }
@@ -117,16 +141,52 @@ function changeSortBy() {
 function removeTodos(event) {
   let tempId = event.target.id.split("-").pop().trim();
   tempId = parseInt(tempId);
-  todoArray.splice(tempId, 1);
+
+  todoArray[tempId].done = true;
+  // todoArray.splice(tempId, 1);
+  saveLocalstorage(todoArray);
   printTodos(todoArray);
 }
 
 function printTodos(todoArray) {
   tasks.innerHTML = "";
+  tasksDone.innerHTML = "";
   let tempTodo = ``;
+  let tempTodo2 = ``;
+  if (todoArray.filter((e) => e.done === true).length > 0) {
+    tasksDone.innerHTML = "<span>Completed</span>";
+  }
   for (i = 0; i < todoArray.length; i++) {
-    tempTodo = `
-    <div class="task mb-2">
+    let statusdone = "";
+    let crossed = "";
+    if (todoArray[i].done === true) {
+      statusdone = "statusdone";
+      crossed = "crossed";
+      tempTodo2 = `
+      <div class="task mb-2 ${statusdone}">
+        <div class="row">
+  
+          <div class="col-2 d-flex justify-content-center align-items-center">
+            <input id="checkbox-${i}" type="checkbox" class="checkmark" checked/>
+          </div>
+  
+          <div class="col-7">
+            <p class="${crossed}">${todoArray[i].text}</p>
+            <time>${todoArray[i].deadline}</time>
+          </div>
+  
+          <div class="col-3 d-flex align-items-end">
+            <a class="priority delete">Remove</a>
+          </div>
+  
+         </div>
+      </div>
+      `;
+      tasksDone.innerHTML += tempTodo2;
+    }
+    if (todoArray[i].done === false) {
+      tempTodo = `
+    <div class="task mb-2 ${statusdone}">
 	    <div class="row">
 
         <div class="col-2 d-flex justify-content-center align-items-center">
@@ -134,7 +194,7 @@ function printTodos(todoArray) {
         </div>
 
 		    <div class="col-7">
-		    	<p>${todoArray[i].text}</p>
+		    	<p class="${crossed}">${todoArray[i].text}</p>
 			    <time>${todoArray[i].deadline}</time>
 		    </div>
 
@@ -142,13 +202,32 @@ function printTodos(todoArray) {
           <span class="priority ${prioScale[
             parseInt(todoArray[i].priority) - 1
           ].toLowerCase()}">${
-      prioScale[parseInt(todoArray[i].priority) - 1]
-    }</span>
+        prioScale[parseInt(todoArray[i].priority) - 1]
+      }</span>
         </div>
 
  	    </div>
     </div>
     `;
-    tasks.innerHTML += tempTodo;
+      tasks.innerHTML += tempTodo;
+    }
+    document.querySelectorAll(".checkmark").forEach((item) => {
+      item.addEventListener("click", (event) => {
+        let tempId = event.target.id.split("-").pop().trim();
+        if (todoArray[tempId].done == true) {
+          todoArray[tempId].done = false;
+          saveLocalstorage(todoArray);
+          printTodos(todoArray);
+        } else {
+          saveLocalstorage(todoArray);
+          removeTodos(event);
+        }
+      });
+    });
+    saveLocalstorage(todoArray);
   }
+}
+
+function saveLocalstorage(todoArray) {
+  localStorage.setItem("todo", JSON.stringify(todoArray));
 }
